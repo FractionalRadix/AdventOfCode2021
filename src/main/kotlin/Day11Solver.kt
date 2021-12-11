14ab9c3949881762e89f1a48ae741f787714a662
FractionalRadix <nerostjust@gmail.com>
2021-12-11 09:11:31 +0100

    Add solution for day 11.

--- src/main/kotlin/Day11Solver.kt
+++ src/main/kotlin/Day11Solver.kt
@@ -4,21 +4,91 @@ import kotlin.io.path.readLines
 
 fun solveDay11() {
     val inputLines =  Path("""inputFiles\AoCDay11.txt""").readLines()
-    val parsedInput = parseInputDay11(inputLines)
+    var parsedInput = parseInputDay11(inputLines)
 
-    println("Solution to puzzle 1: ${solvePuzzle1(parsedInput)}")
-    println("Solution to puzzle 2: ${solvePuzzle2(parsedInput)}")
+    println("Number of flashes in 100 steps: ${countFlashes(parsedInput)}") // 1647
+
+    parsedInput = parseInputDay11(inputLines)
+    println("First step in which all octopuses flash: ${firstSynchronizedFlash(parsedInput)}") // 348
 }
 
-fun parseInputDay11(inputLines: List<String>): Any {
-    return Object()
+fun parseInputDay11(inputLines: List<String>): MutableMap<Point, Int> {
+    val grid = mutableMapOf<Point, Int>()
+    for (y in inputLines.indices) {
+        val line = inputLines[y]
+        for (x in line.indices) {
+            val digit = line[x]
+            grid[Point(x,y)] = digit - '0'
+        }
+    }
+    return grid
 }
 
-fun solvePuzzle1(arg: Any): Long {
-    return 0
+fun countFlashes(grid: MutableMap<Point, Int>): Long {
+    var flashes: Long = 0
+
+    for (i in 1 .. 100) {
+        flashes += step(grid)
+        //print(grid)
+    }
+
+    return flashes
 }
 
-fun solvePuzzle2(arg: Any): Long {
-    return 0
+fun print(grid: MutableMap<Point, Int>) {
+    println()
+    for (y in 0..9) {
+        for (x in 0..9) {
+            print(grid[Point(x,y)])
+        }
+        println()
+    }
+}
+
+fun step(grid: MutableMap<Point, Int>): Long {
+    // First, increase every position by 1.
+    grid.keys.forEach { grid[it] = grid[it]!! + 1 }
+
+    // Second, if any octopus reaches 10, increase its neighbours.
+    val flashers = mutableSetOf<Point>()
+    do {
+        val newFlashers = grid.keys.filter { grid[it]!! > 9 }.toMutableSet() - flashers
+        newFlashers.forEach { increaseNeighbours(grid, it) }
+        flashers.addAll(newFlashers)
+    } while (newFlashers.isNotEmpty())
+
+    // Finally, reset the energy level of all flashers to 0.
+    flashers.forEach { grid[it] = 0 }
+
+    return flashers.size.toLong()
+}
+
+fun increaseNeighbours(grid: MutableMap<Point, Int>, position: Point) {
+    getNeighbours(grid, position).forEach { grid[it] = grid[it]!! + 1 }
+}
+
+fun getNeighbours(grid: MutableMap<Point, Int>, position: Point): List<Point> {
+    val neighbours = mutableListOf<Point>()
+    for (dx in -1 .. +1) {
+        for (dy in -1 .. +1) {
+            if (dx != 0 || dy != 0) {
+                val neighbour = Point(position.x + dx, position.y + dy)
+                if (grid[neighbour] != null) {
+                    neighbours.add(neighbour)
+                }
+            }
+        }
+    }
+    return neighbours
+}
+
+fun firstSynchronizedFlash(grid: MutableMap<Point, Int>): Long {
+    var counter = 0
+    do {
+        val count = step(grid)
+        counter++
+    } while (count < 100)
+
+    return counter.toLong()
 }
 
--- src/test/kotlin/Day11Tests.kt
+++ src/test/kotlin/Day11Tests.kt
@@ -1,33 +1,62 @@
 import org.junit.Before
 import org.junit.Test
+import java.awt.Point
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 import kotlin.test.fail
 
 class Day11Tests {
     private lateinit var inputLines: List<String>
-    private lateinit var parsedInput: Any
+    private lateinit var parsedInput: MutableMap<Point, Int>
 
     @Before
     fun init() {
-        inputLines = listOf()
+        inputLines = listOf(
+            "5483143223",
+            "2745854711",
+            "5264556173",
+            "6141336146",
+            "6357385478",
+            "4167524645",
+            "2176841721",
+            "6882881134",
+            "4846848554",
+            "5283751526",
+        )
         parsedInput = parseInputDay11(inputLines)
     }
 
     @Test
     fun testParseInput() {
         parsedInput = parseInputDay11(inputLines)
-        fail("Not yet implemented")
+        assertEquals(5, parsedInput[Point(0,0)])
+        assertEquals(6, parsedInput[Point(6,3)])
+        assertEquals(4, parsedInput[Point(2,1)])
     }
 
     @Test
-    fun testPuzzle1() {
-        fail("Not yet implemented")
-        assertEquals(0, solvePuzzle1(parsedInput))
+    fun testNeighbours() {
+        parsedInput = parseInputDay11(inputLines)
+        val neighbours = getNeighbours(parsedInput, Point(5,5))
+        assertEquals(8, neighbours.size)
+        assertContains(neighbours, Point(4,4))
+        assertContains(neighbours, Point(4,5))
+        assertContains(neighbours, Point(4,6))
+        assertContains(neighbours, Point(5,4))
+        assertContains(neighbours, Point(5,6))
+        assertContains(neighbours, Point(6,4))
+        assertContains(neighbours, Point(6,5))
+        assertContains(neighbours, Point(6,6))
     }
 
     @Test
-    fun testPuzzle2() {
-        fail("Not yet implemented")
-        assertEquals(0, solvePuzzle2(parsedInput))
+    fun testCountFlashes() {
+        assertEquals(1656, countFlashes(parsedInput))
+    }
+
+    @Test
+    fun testFindSynchronizedFlash() {
+        parsedInput = parseInputDay11(inputLines)
+        assertEquals(195, firstSynchronizedFlash(parsedInput))
     }
 }
