6b45ea2fa99293e2203096b104267c6750c475ce
FractionalRadix <nerostjust@gmail.com>
2021-12-14 21:16:02 +0100

    Add solution for second part of day 14.

--- src/main/kotlin/Day14Solver.kt
+++ src/main/kotlin/Day14Solver.kt
@@ -11,18 +11,124 @@ fun solveDay14() {
     }
     println("Count of most common element, minus count of least common element: ${maxMinusMin(result1)}")  // 3555
 
-    var result2 = template
+    val polymerData = PolymerData()
+    polymerData.init(template)
     for (i in 1 .. 40) {
-        result2 = applyRules(result2, rules)
+        polymerData.applyRuleset(rules)
     }
-    println("Count of most common element, minus count of least common element: ${maxMinusMin(result2)}")
 
+
+    // 1964728807506  - is too low.
+    // 4439442043739 - is the right answer.
+
+    println("Count of most common element, minus count of least common element, after 40 iterations: ${polymerData.maxMinusMin()}")
+}
+
+/**
+ * For the rewriting rules of Advent of Code 14, 2021: a class that keeps track of the digrams and single letters in each application of the ruleset.
+ */
+class PolymerData {
+    private var digramCounts = mapOf<String, Long>()
+    private var letterCounts = mapOf<Char, Long>()
+
+    fun init(template: String) {
+        letterCounts = template
+            .groupingBy { it }
+            .eachCount()
+            .castCharMapToLongValues()
+
+        digramCounts = template
+            .windowed(2)
+            .groupingBy { it }
+            .eachCount()
+            .castStringMapToLongValues()
+    }
+
+    fun applyRuleset(rules: List<Rule>) {
+        val digramCountDelta = mutableMapOf<String, Long>()
+        val letterCountDelta = mutableMapOf<Char, Long>()
+
+        // Apply the rule-set
+        for (rule in rules) {
+            // Lefthand side of the current production rule.
+            val lhs = "${rule.char1}${rule.char2}"
+
+            val digramCount = digramCounts[lhs] ?: 0
+
+            // Righthand side of the current production rule.
+            // (The rule "NN->C" is interpreted as... "NN->NC,CN").
+            val rhs1 = "${rule.char1}${rule.char3}"
+            val rhs2 = "${rule.char3}${rule.char2}"
+
+            digramCountDelta[rhs1] = ( digramCountDelta[rhs1] ?: 0 ) + digramCount
+            digramCountDelta[rhs2] = ( digramCountDelta[rhs2] ?: 0 ) + digramCount
+            digramCountDelta[lhs]   = ( digramCountDelta[lhs  ] ?: 0 ) - digramCount
+
+            letterCountDelta[rule.char3] = (letterCountDelta[rule.char3] ?: 0) + digramCount
+        }
+
+        // Apply the deltas.
+        digramCounts = addStringCounts(digramCounts, digramCountDelta)
+        letterCounts = addCharCounts(letterCounts, letterCountDelta)
+
+        //digramCounts.printStringCounts()
+        letterCounts.printCharCounts()
+    }
+
+    fun letterCount(ch: Char) = letterCounts[ch] ?: 0
+
+    fun maxMinusMin(): Long {
+        val max = letterCounts.maxByOrNull { it.value }!!.value
+        val min = letterCounts.minByOrNull { it.value }!!.value
+
+        letterCounts.printCharCounts()
+
+        return (max - min)
+    }
+}
+
+/**
+ * Given a map of Strings to Ints, cast it to a map of Strings to Longs.
+ * @return A copy of the input mapping, but the values are stored in Longs instead of Ints.
+ */
+fun Map<String,Int>.castStringMapToLongValues(): Map<String, Long> {
+    val result = mutableMapOf<String, Long>()
+    for (entry in this) {
+        result[entry.key] = entry.value.toLong()
+    }
+    return result
+}
+
+fun Map<Char, Int>.castCharMapToLongValues(): Map<Char, Long> {
+    val result = mutableMapOf<Char, Long>()
+    for (entry in this) {
+        result[entry.key] = entry.value.toLong()
+    }
+    return result
+}
+
+/**
+ * Print a mapping from Strings to Longs.
+ */
+fun Map<String, Long>.printStringCounts() {
+    this.forEach {
+        println("${it.key} -> ${it.value}")
+    }
+}
+
+/**
+ * Print a mapping from Chars to Longs.
+ */
+fun Map<Char, Long>.printCharCounts() {
+    this.forEach {
+        println("${it.key} -> ${it.value}")
+    }
 }
 
 fun maxMinusMin(input: String): Long {
     val letterCounts = input.toCharArray().toList().groupingBy { it }.eachCount()
     //val max = letterCounts.maxByOrNull { it.value }
-   // val min = letterCounts.minByOrNull { it.value }
+    // val min = letterCounts.minByOrNull { it.value }
     val max: Long = letterCounts.maxOf { it.value }.toLong()
     val min: Long = letterCounts.minOf { it.value }.toLong()
     return max - min
@@ -54,13 +160,63 @@ class Rule(val char1: Char, val char2: Char, val char3: Char) {
     }
 }
 
+/**
+ * Given a mapping from Strings to Longs, and a list of rewriting rules for these Strings,
+ * determine the <em>change</em> of occurrences of these strings, if these rewriting rule are
+ * applied simultaneously to the same input.
+ */
+fun determineChangeInStringCounts(stringCounts: Map<String,Long>, rules: List<Rule>): MutableMap<String, Long> {
+    val delta = mutableMapOf<String, Long>()
+
+    for (rule in rules) {
+        val input = "${rule.char1}${rule.char2}"
+
+        val count = stringCounts[input] ?: 0
+
+        val output1 = "${rule.char1}${rule.char3}"
+        val output2 = "${rule.char3}${rule.char2}"
+
+        delta[output1] = ( delta[output1] ?: 0 ) + count
+        delta[output2] = ( delta[output2] ?: 0 ) + count
+        delta[input]   = ( delta[input  ] ?: 0 ) - count
+    }
+
+    return delta
+}
+
+/**
+ * Given two mappings that map Strings to counts, add these maps together.
+ * For example, if the maps are { "A"->3, "B"->2 } and { "A"->4,"C"->5 }, the result would be { "A"->7, "B"->2, "C"->5 }.
+ * We allow for the possibility that a count value is negative. This is because one or both of the maps might represent a delta, rather than a count.
+ * @param map1 A mapping from Strings to Longs. The Long represents a number of occurrences of the String.
+ * @param map2 A mapping from Strings to Longs. The Long represents a number of occurrences of the String.
+ * @return A mapping from Strings to Longs. The Long represents the summed number of occurrences of the String over the input maps.
+ */
+fun addStringCounts(map1: Map<String, Long>, map2: Map<String, Long>): Map<String, Long> {
+    val sum = mutableMapOf<String, Long>()
+    val keys = map1.keys.union(map2.keys)
+    for (key in keys) {
+        sum[key] = (map1[key] ?: 0) + (map2[key] ?: 0)
+    }
+    return sum
+}
+
+fun addCharCounts(map1: Map<Char, Long>, map2: Map<Char, Long>): Map<Char, Long> {
+    val sum = mutableMapOf<Char ,Long>()
+    val keys = map1.keys.union(map2.keys)
+    for (key in keys) {
+        sum[key] = (map1[key] ?: 0) + (map2[key] ?: 0)
+    }
+    return sum
+}
+
 fun applyRules(template: String, rules: List<Rule>): String {
     // First, for every rule, check if it matches.
     // Find all occurrences of AB in the string.
 
     val splitString = template.windowed(2).toMutableList()
-    splitString.forEach { print(" ${it}")}
-    println()
+    //splitString.forEach { print(" ${it}")}
+    //println()
 
     for (rule in rules) {
         val input = "${rule.char1}${rule.char2}"
@@ -90,7 +246,7 @@ fun parseInputDay14(inputList: List<String>): Pair<String, List<Rule>> {
         val ch2 = it[1]
         val ch3 = it[6]
         val rule = Rule(ch1, ch2, ch3)
-        rule.print()
+        //rule.print()
         parsedRules.add(rule)
     }
     return Pair(template, parsedRules)
--- src/test/kotlin/Day14Tests.kt
+++ src/test/kotlin/Day14Tests.kt
@@ -55,4 +55,111 @@ class Day14Tests {
         }
         Assert.assertEquals(1588, maxMinusMin(template))
     }
+
+    @Test
+    fun testApplyRules2Once() {
+        val (template, rules) = parseInputDay14(inputLines)
+
+        val map = template
+            .windowed(2)
+            .groupingBy { it }
+            .eachCount()
+            .castStringMapToLongValues()
+
+        // NNCB -> NCNBCHB
+        // NN, NC, CB -> NC, CN, NB, BC, CH, HB
+        val delta = determineChangeInStringCounts(map, rules)
+        val result = addStringCounts(map, delta)
+        Assert.assertEquals(1L, result["NC"])
+        Assert.assertEquals(1L, result["CN"])
+        Assert.assertEquals(1L, result["NB"])
+        Assert.assertEquals(1L, result["BC"])
+        Assert.assertEquals(1L, result["CH"])
+        Assert.assertEquals(1L, result["HB"])
+
+        Assert.assertEquals(6L, result.values.count { it != 0L }.toLong() )
+    }
+
+    @Test
+    fun testApplyUsingPolymerDataClass_1iteration() {
+        val (template, rules) = parseInputDay14(inputLines)
+        val polymerData = PolymerData()
+        polymerData.init(template)
+
+        polymerData.applyRuleset(rules)
+
+        // 1 iterations:
+        //  NCNBCHB  N=2 C=2 B=2 H=1
+        Assert.assertEquals(2, polymerData.letterCount('N'))
+        Assert.assertEquals(2, polymerData.letterCount('B'))
+        Assert.assertEquals(2, polymerData.letterCount('C'))
+        Assert.assertEquals(1, polymerData.letterCount('H'))
+    }
+
+    @Test
+    fun testApplyUsingPolymerDataClass_2iterations() {
+        val (template, rules) = parseInputDay14(inputLines)
+        val polymerData = PolymerData()
+        polymerData.init(template)
+        for (i in 1 .. 2) {
+            polymerData.applyRuleset(rules)
+        }
+
+        // 2 iterations:
+        // NBCCNBBBCBHCB N=2 B=6 H=1 C=4
+        Assert.assertEquals(2, polymerData.letterCount('N'))
+        Assert.assertEquals(6, polymerData.letterCount('B'))
+        Assert.assertEquals(4, polymerData.letterCount('C'))
+        Assert.assertEquals(1, polymerData.letterCount('H'))
+    }
+
+    @Test
+    fun testApplyUsingPolymerDataClass_3iterations() {
+        val (template, rules) = parseInputDay14(inputLines)
+        val polymerData = PolymerData()
+        polymerData.init(template)
+        for (i in 1 .. 3) {
+            polymerData.applyRuleset(rules)
+        }
+
+        // 3 iterations:
+        //  NBBBCNCCNBBNBNBBCHBHHBCHB  N=5 B=11 C=5 H=4
+        Assert.assertEquals(5, polymerData.letterCount('N'))
+        Assert.assertEquals(11, polymerData.letterCount('B'))
+        Assert.assertEquals(5, polymerData.letterCount('C'))
+        Assert.assertEquals(4, polymerData.letterCount('H'))
+    }
+
+    @Test
+    fun testApplyUsingPolymerDataClass_4iterations() {
+        val (template, rules) = parseInputDay14(inputLines)
+        val polymerData = PolymerData()
+        polymerData.init(template)
+        for (i in 1 .. 4) {
+            polymerData.applyRuleset(rules)
+        }
+
+
+        // 4 iterations:
+        // NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB N=11 B=25 H=5 C=10
+
+        Assert.assertEquals(11, polymerData.letterCount('N'))
+        Assert.assertEquals(23, polymerData.letterCount('B'))
+        Assert.assertEquals(10, polymerData.letterCount('C'))
+        Assert.assertEquals(5, polymerData.letterCount('H'))
+    }
+
+    @Test
+    fun testApplyUsingPolymerDataClass_40iterations() {
+        val (template, rules) = parseInputDay14(inputLines)
+        val polymerData = PolymerData()
+        polymerData.init(template)
+        for (i in 1 .. 40) {
+            polymerData.applyRuleset(rules)
+        }
+
+        val result = polymerData.maxMinusMin()
+        Assert.assertEquals(2188189693529, result)
+    }
+
 }
